87786853bc301d3482ba5ade274d7a0d2ee4b98e
harsh11-11 <h7jain@gmail.com>
2023-06-15 04:35:33 +0530

    new changes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # affliate
 affiliate Amazon
+
+have edge case in amazon
+https://www.amazon.in/Canon-EOS-200D-II-Digital/dp/B07RJWB548?crid=3LGHVRTI4U03U&keywords=canon%2B200d%2Bmark%2Bii%2B55-250mm%2Blens&qid=1676437536&sprefix=canon%2B200d,aps,226&sr=8-1&th=1&linkCode=sl1&tag=r09d5-21&linkId=42fc5bf9044797ef9c4e66e44ab7307b&language=en_IN&ref_=as_li_ss_tl
+
+https://fkrt.it/F0wHJRuuuN
--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 function CreateURL(tag,subtag, url){
-  //  alert(url);
-  const amazonRegex = /^(https?:\/\/)?(www\.)?amazon\.[a-z]{2,}\/(?:[\w-]+\/)?(dp|gp\/product)\/([\w]{10})(\/.*)?$/;
+  //const amazonRegex = /^(https?:\/\/)?(www\.)?amazon\.[a-z]{2,}\/(?:[\w-]+\/)?(dp|gp\/product)\/([\w]{10})(\/.*)?$/;
+  const amazonRegex = /^(https?:\/\/)?(www\.)?amazon|amzn\.[a-z]{2,}\/.*$/; 
+  const flipkartRegex = /^(https?:\/\/)?(www\.)?flipkart|fkrt\.[a-z]{2,}\/.*$/;
+
   if(amazonRegex.test(url)){
+    alert("enter in amazon");
     var ASINreg = new RegExp(/(?:\/)([A-Z0-9]{10})(?:$|\/|\?)/);
+    
+    if(!url &&(!tag || !subtag)) {
+      return alert(`Please provide a url and your amazon tracking code. Example: CreateURL('amazon.com/productblahblah', 'pickitly0b-20')`)
+    }
+    var  cMatch = url.match(ASINreg); //matches productID and url's productID
+    if(cMatch == null){
+        return null;
+    }
+
+    let index = url.indexOf("&tag=");
+    if(index !== -1){
+      let modifyUrl = url.slice(0,index+5); //whole string before '&tag='
+      let newUrl = url.slice(index+5, url.length); //whole string after '&tag='
+      let newIndex = newUrl.indexOf('&'); //index before '&' tag
+      let otherPart = url.slice(index + 5 + newIndex + 1, url.length); //whole string after &
+      var generated = modifyUrl + `${tag}`+"&"+  otherPart +`&ascsubtag=${subtag}`;
+      document.getElementById('result').textContent = generated;
+      return;
+    }
+    else{
+      var generated = url + `&tag=${tag}&ascsubtag=${subtag}`;
+      document.getElementById('result').textContent = generated;
+    }  
+  }
+ 
+
+  if(flipkartRegex.test(url)){
+    alert("enter in flipkart");
+    var ASINreg = new RegExp(/(?:pid=)([\w]+)/);
     if(!url &&(!tag || !subtag)) {
       return alert(`Please provide a url and your amazon tracking code. Example: CreateURL('amazon.com/productblahblah', 'pickitly0b-20')`)
     }
@@ -10,20 +42,25 @@ function CreateURL(tag,subtag, url){
     if(cMatch == null){
         return null;
     }
+    
 
-    let index = url.indexOf("&tag=");
+    let index = url.indexOf("&affid=");
     if(index !== -1){
-      let modifyUrl = url.slice(0,index);
-      // var asin =  cMatch[0];
-      var generated = modifyUrl + `&tag=${tag}&ascsubtag=${subtag}`;
+      let modifyUrl = url.slice(0,index+5); //whole string before '&tag='
+      let newUrl = url.slice(index+5, url.length); //whole string after '&tag='
+      let newIndex = newUrl.indexOf('&'); //index before '&' tag
+      let otherPart = url.slice(index + 5 + newIndex + 1, url.length); //whole string after &
+      var generated = modifyUrl + `${tag}`+"&"+  otherPart +`&affExtParam1=${subtag}`;
       document.getElementById('result').textContent = generated;
+      return;
     }
     else{
-      // var asin =  cMatch[0];
-      var generated = url + `&tag=${tag}&ascsubtag=${subtag}`;
+      var generated = url + `&affid=${tag}&affExtParam1=${subtag}`; //there should be &affid= or ?affid=?
       document.getElementById('result').textContent = generated;
     }  
   }
+
+  
   else{
     console.log('enter valid url');
     return;
